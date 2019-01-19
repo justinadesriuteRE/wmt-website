7dba45bc3d59297e737f94075e8ff5f08d1e4e7d
Justina Desriute <justina.desriute@netcentric.biz>
2019-01-19 17:17:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './wmt.png';
 import './App.css';
 
 class App extends Component {
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome to WomenTechMakers Frankfurt!
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.linkedin.com/company/women-techmakers-frankfurt/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Meet us!
           </a>
         </header>
       </div>
